6f6e164b65286df1e8e0fa65acf35237b0ae1616
matheusysd <matheusyurisd@gmail.com>
2020-09-28 23:40:38 -0300

    Updates

---
+++ precache-manifest.45c88b2427e0792fd1719b32d9c2e2fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6604580d9566c7e68cdd19b43d26a756",
+    "url": "/project-trivia-redux-Trybe/index.html"
+  },
+  {
+    "revision": "34011aab30818d233758",
+    "url": "/project-trivia-redux-Trybe/static/css/2.c31a3e37.chunk.css"
+  },
+  {
+    "revision": "135c2a8c3099adc12b81",
+    "url": "/project-trivia-redux-Trybe/static/css/main.81a16442.chunk.css"
+  },
+  {
+    "revision": "34011aab30818d233758",
+    "url": "/project-trivia-redux-Trybe/static/js/2.8270ab59.chunk.js"
+  },
+  {
+    "revision": "b9da430482e3f0eb230634f9fc6c20c5",
+    "url": "/project-trivia-redux-Trybe/static/js/2.8270ab59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "135c2a8c3099adc12b81",
+    "url": "/project-trivia-redux-Trybe/static/js/main.58b9fc62.chunk.js"
+  },
+  {
+    "revision": "a31db4ab1d10d2b990da",
+    "url": "/project-trivia-redux-Trybe/static/js/runtime-main.593b55a2.js"
+  },
+  {
+    "revision": "943ba04b6d89e71a1f38bf0bd4826942",
+    "url": "/project-trivia-redux-Trybe/static/media/2316767.943ba04b.jpg"
+  }
+]);
